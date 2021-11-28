b5cdd2e49cfabb854576ea41c39fe42c3e23e19a
Hiresh <Hiresh3p@gmail.com>
2021-11-28 20:15:39 +0400

    -rm Hero

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import "./Bootstrap/bootstrap.min.css";
 import "./App.css";
 import Header from "./Components/Header/Header";
-import HeroSection from "./Components/HeroSection/HeroSection";
 import ServiceSection from "./Components/ServiceSection/ServiceSection";
 
 function App() {
   return (
     <>
       <Header />
-      {/* <HeroSection /> */}
       <ServiceSection />
     </>
   );
